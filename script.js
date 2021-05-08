4f8596460b91dbe0dd60a2ab860181b47ea2d61b
AnindyaAB <biswas.anindya6@gmail.com>
2021-05-09 00:04:12 +0300

    to load faces

--- script.js
+++ script.js
@@ -1,35 +1,81 @@
-const video = document.getElementById("video")
+// const video = document.getElementById("video")
+
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+//   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+//   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+//   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+// ]).then(startVideo)
+
+// function startVideo() {
+//   navigator.getUserMedia(
+//     { video: {} },
+//     stream => (video.srcObject = stream),
+//     error => console.log(error)
+//   )
+// }
+
+// video.addEventListener("play", () => {
+//   const canvas = faceapi.createCanvasFromMedia(video)
+//   document.body.append(canvas)
+//   const displaySize = { width: video.width, height: video.height }
+//   faceapi.matchDimensions(canvas, displaySize)
+//   setInterval(async () => {
+//     const detection = await faceapi
+//       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+//       .withFaceLandmarks()
+//       .withFaceExpressions()
+//     console.log(detection)
+//     const resizeddetection = faceapi.resizeResults(detection, displaySize)
+//     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height)
+//     faceapi.draw.drawdetection(canvas, resizeddetection)
+//     faceapi.draw.drawFaceLandmarks(canvas, resizeddetection)
+//     faceapi.draw.drawFaceExpressions(canvas, resizeddetection)
+//   }, 100)
+// })
+
+const imageUpload = document.getElementById("imageUpload")
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-]).then(startVideo)
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+]).then(start)
 
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video.srcObject = stream),
-    error => console.log(error)
-  )
+function start() {
+  const container = document.createElement("div")
+  container.style.position = "relative"
+  document.body.append(container)
+  imageUpload.addEventListener("change", async () => {
+    const image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    const canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detection = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors()
+    document.body.append("Number of faces :", detection.length)
+    const resizeddetection = faceapi.resizeResults(detection, displaySize)
+    resizeddetection.forEach(detection => {
+      const box = detection.detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: "FACE" })
+      drawBox.draw(canvas)
+    })
+  })
 }
 
-video.addEventListener("play", () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions()
-    console.log(detections)
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+function loadLabledImages() {
+  const labels = ["Anindya", "Hug Diya"]
+  return Promise.all(
+    labels.map(async label => {
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(
+          `https://drive.google.com/drive/folders/1iKsxbdgOE-N_6UPlvL5eGeW0WHN6QKHk?usp=sharing/${label}/${i}.jpg`
+        )
+      }
+    })
+  )
+}
