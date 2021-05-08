991a798bd16d55d34db27e8b3ead52bfb385bedf
AnindyaAB <biswas.anindya6@gmail.com>
2021-05-09 01:16:40 +0300

    added more images

--- script.js
+++ script.js
@@ -34,6 +34,8 @@
 //   }, 100)
 // })
 
+// Program 2
+
 const imageUpload = document.getElementById("imageUpload")
 
 Promise.all([
@@ -46,26 +48,30 @@ async function start() {
   const container = document.createElement("div")
   container.style.position = "relative"
   document.body.append(container)
-  const labledFaceDescriptors = await loadLabledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labledFaceDescriptors, 0.6)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append("Loaded")
   imageUpload.addEventListener("change", async () => {
-    const image = await faceapi.bufferToImage(imageUpload.files[0])
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
     container.append(image)
-    const canvas = faceapi.createCanvasFromMedia(image)
+    canvas = faceapi.createCanvasFromMedia(image)
     container.append(canvas)
     const displaySize = { width: image.width, height: image.height }
     faceapi.matchDimensions(canvas, displaySize)
-    const detection = await faceapi
+    const detections = await faceapi
       .detectAllFaces(image)
       .withFaceLandmarks()
       .withFaceDescriptors()
-    document.body.append("Number of faces :", detection.length)
-    const resizeddetection = faceapi.resizeResults(detection, displaySize)
-    const results = resizeddetection.map(d =>
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d =>
       faceMatcher.findBestMatch(d.descriptor)
     )
-    results.forEach((results, i) => {
-      const box = resizeddetection[i].detection.box
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
       const drawBox = new faceapi.draw.DrawBox(box, {
         label: result.toString(),
       })
@@ -73,25 +79,96 @@ async function start() {
     })
   })
 }
-
-function loadLabledImages() {
-  const labels = ["Anindya", "HugDiya"]
+function loadLabeledImages() {
+  const labels = [
+    "Anindya",
+    "HugDiya",
+    "HugDiya2",
+    "HugDiya3",
+    "HugDiya4",
+    "HugDiya5",
+    "HugDiya6",
+  ]
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
         const img = await faceapi.fetchImage(
-          `https://github.com/AnindyaAB/FaceDetector/tree/main/labeled_images/${label}/${i}.jpg`
+          `https://raw.githubusercontent.com/AnindyaAB/FaceDetector/main/labeled_images/${label}/${i}.jpg`
         )
 
         const detections = await faceapi
           .detectSingleFace(img)
           .withFaceLandmarks()
-          .withFaceDescriptors()
+          .withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
-
       return new faceapi.LabledFaceDescriptors(label, descriptions)
     })
   )
 }
+
+// const imageUpload = document.getElementById("imageUpload")
+
+// Promise.all([
+//   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+//   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+//   faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+// ]).then(start)
+
+// async function start() {
+//   const container = document.createElement("div")
+//   container.style.position = "relative"
+//   document.body.append(container)
+//   const labeledFaceDescriptors = await loadLabeledImages()
+//   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+//   let image
+//   let canvas
+//   document.body.append("Loaded")
+//   imageUpload.addEventListener("change", async () => {
+//     if (image) image.remove()
+//     if (canvas) canvas.remove()
+//     image = await faceapi.bufferToImage(imageUpload.files[0])
+//     container.append(image)
+//     canvas = faceapi.createCanvasFromMedia(image)
+//     container.append(canvas)
+//     const displaySize = { width: image.width, height: image.height }
+//     faceapi.matchDimensions(canvas, displaySize)
+//     const detections = await faceapi
+//       .detectAllFaces(image)
+//       .withFaceLandmarks()
+//       .withFaceDescriptors()
+//     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+//     const results = resizedDetections.map(d =>
+//       faceMatcher.findBestMatch(d.descriptor)
+//     )
+//     results.forEach((result, i) => {
+//       const box = resizedDetections[i].detection.box
+//       const drawBox = new faceapi.draw.DrawBox(box, {
+//         label: result.toString(),
+//       })
+//       drawBox.draw(canvas)
+//     })
+//   })
+// }
+
+// function loadLabeledImages() {
+//   const labels = ["Anindya", "HugDiya"]
+//   return Promise.all(
+//     labels.map(async label => {
+//       const descriptions = []
+//       for (let i = 1; i <= 2; i++) {
+//         const img = await faceapi.fetchImage(
+//           `https://raw.githubusercontent.com/AnindyaAB/FaceDetector/main/labeled_images/${label}/${i}.jpg`
+//         )
+//         const detections = await faceapi
+//           .detectSingleFace(img)
+//           .withFaceLandmarks()
+//           .withFaceDescriptor()
+//         descriptions.push(detections.descriptor)
+//       }
+
+//       return new faceapi.LabeledFaceDescriptors(label, descriptions)
+//     })
+//   )
+// }
