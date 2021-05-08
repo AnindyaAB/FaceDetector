6f0d9c83032eff20b64ee20d773be0da84453833
AnindyaAB <biswas.anindya6@gmail.com>
2021-05-09 01:59:13 +0300

    added more images

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ["Anindya", "Subham"]
+  const labels = ["Anindya", "Subham", "BOT AF"]
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
